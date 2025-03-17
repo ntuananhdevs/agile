259a142ba252aac52d7ea5194f912666a4d3b886
ntuananhdevs <anhndt25@gmail.com>
2025-03-17 16:09:31 +0700

    firts commit

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-require 'vendor/autoload.php';
-
-Dotenv\Dotenv::createImmutable(__DIR__)->load();
-
-require 'routes/index.php';
--- routes/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Create Router instance
-$router = new \Bramus\Router\Router();
-
-// viết router ở đây
-
-
-// Run it!
-$router->run();
