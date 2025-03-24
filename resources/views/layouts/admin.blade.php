e2e01fb545d9ed9a20d681e1a4d1b20552104573
ntuananhdevs <anhndt25@gmail.com>
2025-03-24 22:24:54 +0700

    add button logout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -174,5 +174,7 @@
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
