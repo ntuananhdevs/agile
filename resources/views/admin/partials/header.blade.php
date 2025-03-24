390152e4bbcad661d37f79023208c7a8835bc823
ntuananhdevs <anhndt25@gmail.com>
2025-03-24 21:56:48 +0700

    delete search header

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -3,12 +3,7 @@
       <i class="text-primary fw-bold">ADMIN DEP ZAI 💕🙌😎</i>
   </div>
 
-  <div class="d-flex align-items-center">
-    <form class="d-flex ms-3" action="" method="GET">
-      <input class="form-control-search border-secondary me-2" type="search" name="key" placeholder="Tìm kiếm" aria-label="Search">
-      <button class="btn btn-outline-secondary" type="submit">Tìm</button>
-  </form>
-  
+  <div class="d-flex align-items-center">  
       <img src="{{ asset('asset/img/user.png') }}" alt="User" class="img-circle ms-3">
   </div>
 </div>
